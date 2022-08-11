5a980f239ee4d65100b42820d17d1d4126e796c9
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-08-11 18:41:20 -0400

    updated crossOrigin

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
         <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&family=Jost&family=Koulen&family=Permanent+Marker&family=Vujahday+Script&display=swap" rel="stylesheet"/>
       </Head>
       <body>
