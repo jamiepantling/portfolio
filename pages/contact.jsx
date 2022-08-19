fb25c1ce8304d12048db3c8f15f6ae4f581fbeda
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-08-19 11:37:59 -0400

    transition, input sizes, nav pointer

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -1,47 +1,80 @@
-import styles from '../styles/contact.module.css'
-import Nav from '../components/nav/nav'
+import styles from "../styles/contact.module.css";
+import Nav from "../components/nav/nav";
 import { useForm } from "react-hook-form";
-import { useState } from 'react';
+import { useState } from "react";
+import { motion } from "framer-motion";
 
+export default function About() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [success, setSuccess] = useState("");
 
-export default function About () {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [success, setSuccess] = useState('')
+  const onSubmit = (data) => {
+    setName(data.name);
+    setEmail(data.email);
+    setMessage(data.message);
+    fetch("./api/contact", {
+      method: "post",
+      body: JSON.stringify(data),
+    });
+    setSuccess("Message sent. Thanks!");
+  };
+  return (
+    <div className={styles.container}>
+      <Nav />
+      <motion.div
+        className={styles.contactContainer}
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          duration: 0.8,
+          delay: 0,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        <h1>Get in touch!</h1>
 
-    const onSubmit = data => {
-        setName(data.name)
-        setEmail(data.email)
-        setMessage(data.message)
-        fetch('./api/contact', {
-            method: 'post',
-            body: JSON.stringify(data),
-          });
-        setSuccess("Message sent. Thanks!")
-    }
-    return (
-        <div className={styles.container}>
-            <Nav/>
-            <div className={styles.contactContainer}>
-                <h1>Get in touch!</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className={styles.inputContainer}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.inputContainer}>
+            <input
+              className={styles.input}
+              placeholder="name..."
+              {...register("name", { required: true })}
+            />
+            <input
+              className={styles.input}
+              placeholder="email..."
+              {...register("email", { required: true })}
+            />
+            <textarea
+              className={`${styles.input} ${styles.textarea}`}
+              placeholder="message..."
+              {...register("message", { required: true })}
+            />
+            {errors.exampleRequired && <span>This field is required</span>}
 
-                        <input className={styles.input} placeholder="name..." {...register("name", { required: true })} />
-                        
-                        {/* include validation with required or other standard HTML validation rules */}
-                        <input className={styles.input} placeholder="email..." {...register("email", { required: true })} />
-                        <textarea className={`${styles.input} ${styles.textarea}`} placeholder="message..." {...register("message", { required: true })} />
-                        {/* errors will return when field validation fails  */}
-                        {errors.exampleRequired && <span>This field is required</span>}
-            
-                        <input className={styles.button} type="submit" />
-                    </div>
-                </form>
-                <p className={styles.success}>{success}</p>
-            </div>
-        </div>
-    )
-} 
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className={styles.button}
+              type="submit"
+            >
+              Submit
+            </motion.button>
+          </div>
+        </form>
+
+        <motion.div layout className={styles.success}>
+          {success}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
