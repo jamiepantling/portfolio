435b76562e99d99d515433675a723071a29d779b
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2022-08-19 12:09:29 -0400

    remove page transition

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,10 @@ import '../styles/globals.css'
 import '../styles/transition.css'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import Transition from '../components/Transition'
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
-  return <Transition><Component {...pageProps} /></Transition>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
