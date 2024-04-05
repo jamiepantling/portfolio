163da7784157889151fc5c3e3a8595165bb8f5fb
Jamie Pantling <99222790+jamiepantling@users.noreply.github.com>
2024-04-05 16:03:33 -0400

    remove deployed for non-deployed games

--- components/modal/modal.jsx
+++ components/modal/modal.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom";
-import styled from "styled-components";
-import styles from "./modal.module.css";
-import { AnimatePresence, motion } from "framer-motion";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { brands, solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import styles from './modal.module.css';
+import { AnimatePresence, motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  brands,
+  solid,
+} from '@fortawesome/fontawesome-svg-core/import.macro';
 
 const Modal = (props) => {
   const [isBrowser, setIsBrowser] = useState(false);
@@ -19,30 +22,31 @@ const Modal = (props) => {
 
   useEffect(() => {
     setIsBrowser(true);
-    window.addEventListener("click", backDropHandler);
+    window.addEventListener('click', backDropHandler);
     return () => {
-      window.removeEventListener("click", backDropHandler);
+      window.removeEventListener('click', backDropHandler);
     };
   }, []);
 
   useEffect(() => {
     const close = (e) => {
-      if (e.key === "Escape") {
+      if (e.key === 'Escape') {
         props.onClose();
       }
     };
-    window.addEventListener("keydown", close);
-    return () => window.removeEventListener("keydown", close);
+    window.addEventListener('keydown', close);
+    return () => window.removeEventListener('keydown', close);
   }, []);
   const handleCloseClick = (e) => {
     e.preventDefault();
     props.onClose();
   };
   const GameName = styled.span`
-    font-family: ${props.font || "cursive"};
-    font-size: ${props.style === "sproutsweeper" || props.style === "boredgame"
-      ? "large"
-      : "x-large"};
+    font-family: ${props.font || 'cursive'};
+    font-size: ${props.style === 'sproutsweeper' ||
+    props.style === 'boredgame'
+      ? 'large'
+      : 'x-large'};
   `;
 
   const modalContent = (
@@ -63,7 +67,9 @@ const Modal = (props) => {
             <div className={styles.modalHeader}>
               <div></div>
               <div className={styles.modalTitle}>
-                <GameName className={props.style}>{props.name}</GameName>
+                <GameName className={props.style}>
+                  {props.name}
+                </GameName>
               </div>
               <motion.button
                 whileHover={{ scale: 1.05 }}
@@ -71,7 +77,7 @@ const Modal = (props) => {
                 className={styles.x}
                 onClick={handleCloseClick}
               >
-                <FontAwesomeIcon icon={solid("xmark")} />
+                <FontAwesomeIcon icon={solid('xmark')} />
               </motion.button>
             </div>
 
@@ -99,18 +105,22 @@ const Modal = (props) => {
                       whileTap={{ scale: 0.9 }}
                       className={`${styles.githubButton} ${styles.button}`}
                     >
-                      <FontAwesomeIcon icon={brands("github")} /> GitHub{" "}
+                      <FontAwesomeIcon icon={brands('github')} />{' '}
+                      GitHub{' '}
                     </motion.button>
                   </a>
-                  <a href={props.deployedUrl}>
-                    <motion.button
-                      whileHover={{ scale: 1.05 }}
-                      whileTap={{ scale: 0.9 }}
-                      className={`${styles.deployedButton} ${styles.button}`}
-                    >
-                      <GameName>{props.name}</GameName>
-                    </motion.button>
-                  </a>
+                  {props.style == 'sproutsweeper' ||
+                  props.style == 'boredgame' ? (
+                    <a href="https://sprout-sweeper.herokuapp.com/">
+                      <motion.button
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.9 }}
+                        className={`${styles.deployedButton} ${styles.button}`}
+                      >
+                        <GameName>{props.name}</GameName>
+                      </motion.button>
+                    </a>
+                  ) : null}
                 </div>
               </div>
             </div>
@@ -123,7 +133,7 @@ const Modal = (props) => {
   if (isBrowser) {
     return ReactDOM.createPortal(
       modalContent,
-      document.getElementById("modal-root")
+      document.getElementById('modal-root')
     );
   } else {
     return null;
